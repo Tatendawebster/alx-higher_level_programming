d5cd2669e68278d7b199568c22d5ed096ffbc118
Twmushaurwi <twmushaurwi@gmail.com>
2024-02-15 16:46:45 -0800

    all tasks

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,3 @@
--- Displays the average temperature (in Fahrenheit) by city ordered by descending temperature.
-SELECT `city`, AVG(`value`) AS `avg_temp`
-FROM `temperatures`
-GROUP BY `city`
-ORDER BY `avg_temp` DESC;
+-- List max value by state
+-- Execute: cat 103-max_state.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
